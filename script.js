3b1c3192068f9a88ac741add10f950dec5875fef
alex6061 <39526012+alex6061@users.noreply.github.com>
2022-11-13 20:42:35 +0300

    1

--- script.js
+++ script.js
@@ -14,17 +14,53 @@ $(function() {
 $(function() {
 
     $('.trainer__wrapper').slick ({
-        // arrows:true,
-
         slidesToShow: 6,
         Infinity: true,
 
 
         prevArrow: '.trainee-button-next',
         nextArrow: '.trainee-button-prev',
-    })
 
-})
+        responsive: [
+            {
+            breakpoint: 1280,
+            settings: {
+                slidesToScroll: 2,
+                slidesToShow: 5,
+            }
+        },
+        {
+            breakpoint: 1000,
+            settings: {
+                slidesToScroll: 2,
+                slidesToShow: 4,
+            }
+        },
+        {
+            breakpoint: 779,
+            settings: {
+                slidesToScroll: 3,
+                slidesToShow: 3,
+            }
+        },
+        {
+            breakpoint: 624,
+            settings: {
+                slidesToScroll: 2,
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint: 328,
+            settings: {
+                slidesToScroll: 1,
+                slidesToShow: 1,
+            }
+        },
+    
+]
+
+})})
 
 
 $(function() {
